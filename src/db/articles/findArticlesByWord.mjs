41302821a113d8869b1d8dad4d20f5a52255ab07
Johan s <48106430+johan12361@users.noreply.github.com>
2026-04-22 18:28:42 -0500

    Update findArticlesByWord.mjs

--- src/db/articles/findArticlesByWord.mjs
+++ src/db/articles/findArticlesByWord.mjs
@@ -13,27 +13,27 @@ export async function findArticlesByWord(word) {
     const searchWord = word.toLowerCase()
 
     // Buscar en el nombre del artículo
-    if (article.descripcion && article.descripcion.toLowerCase().includes(searchWord)) {
+    if (article.descripcion?.toLowerCase().includes(searchWord)) {
       return true
     }
 
     // Buscar en la descripción del artículo
-    if (article.descripcionAvanzada && article.descripcionAvanzada.toLowerCase().includes(searchWord)) {
+    if (article.descripcionAvanzada?.toLowerCase().includes(searchWord)) {
       return true
     }
 
     // Buscar en las familias
-    if (article.familia && article.familia.toLowerCase().includes(searchWord)) {
+    if (article.familia?.toLowerCase().includes(searchWord)) {
       return true
     }
 
     // Buscar en el ramo
-    if (article.ramo && article.ramo.toLowerCase().includes(searchWord)) {
+    if (article.ramo?.toLowerCase().includes(searchWord)) {
       return true
     }
 
     // Buscar en el grupo
-    if (article.grupo && article.grupo.toLowerCase().includes(searchWord)) {
+    if (article.grupo?.toLowerCase().includes(searchWord)) {
       return true
     }
     return false
